56a445fdd04fb9a69d545acbfa590e96c1a6fbff
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2013-12-09 22:26:41 +0100

    Error correction and convenience script.

---
+++ makeResults.sh
@@ -0,0 +1,2 @@
+./makeTex.sh _results1.csv ~/Research/datastructures-for-metaprogramming/profiling-for-sharing/tables/results1_of_profiling_benchmarks_data.tex
+./makeTex.sh _results2.csv ~/Research/datastructures-for-metaprogramming/profiling-for-sharing/tables/results2_of_profiling_benchmarks_data.tex
